c391bba54997ab8c5592ee6d28edd26e8eda3514
JisooShin <d2vr1v2r@gmail.com>
2024-09-27 15:15:30 +0900

    jisoo's portfolio design ~ing

---
+++ server.js
@@ -0,0 +1,13 @@
+// express 라이브러리 사용 코드
+const express = require('express')
+const app = express()
+
+// 서버 띄우기
+app.listen(8080, ()=>{
+    console.log('http://localhost:8080에서 서버 실행 중')
+})
+
+// main page 접속하기
+app.get('/', (request, response)=>{
+    response.send("지수의 포트폴리오에 오신 여러분, 환영합니다.")
+})
